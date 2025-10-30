bf8bb8277b74770464a94dfffbcef3ae51df3cc3
bpinedaf <bpinedaf@gmail.com>
2025-10-30 09:26:39 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Config: API base (termina en /exec). Se guarda en localStorage.
 window.CONFIG = {
-  API: 'https://script.google.com/macros/s/AKfycbxUOVcn6K5anNZohvzxtRTPVSvZfyceH21AiBiMbLU4CrgfQ7c5CDp-SKNZK7UN1sHR/exec' // ej: 'https://script.google.com/macros/s/AKfyc.../exec'
+  //API: 'https://script.google.com/macros/s/AKfycbxUOVcn6K5anNZohvzxtRTPVSvZfyceH21AiBiMbLU4CrgfQ7c5CDp-SKNZK7UN1sHR/exec' // ej: 'https://script.google.com/macros/s/AKfyc.../exec'
+  API: 'https://script.google.com/macros/s/AKfycbylWQu1v_zy6mCpatSFlpxt5wQICtm8hFrR4EQHfSKVfnCf-HLpw3JXvpGn8Gr7_h6_1w/exec' // This is then new one
 };
